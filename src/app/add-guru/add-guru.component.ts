ec48c034f918fb651a86cc119f20edf21b44e08e
Nate <nshirley22@gmail.com>
2020-03-04 16:26:38 -0500

    vanill form done

--- src/app/add-guru/add-guru.component.ts
+++ src/app/add-guru/add-guru.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { Guru } from "../interfaces/guru";
+declare const previewGooVanilla: any;
+declare const submitGooVanilla: any;
 @Component({
   selector: "app-add-guru",
   templateUrl: "./add-guru.component.html",
-  styleUrls: ["./add-guru.component.css"]
+  styleUrls: ["./add-guru.component.css"],
+  encapsulation: ViewEncapsulation.None
 })
 export class AddGuruComponent implements OnInit {
+  onPreview = () => {
+    previewGooVanilla();
+  };
+  onSubmitGoo = () => {
+    submitGooVanilla();
+  };
   constructor() {}
   categories = [
     "Amazon FBA",
--- src/scripts.js
+++ src/scripts.js
@@ -16,17 +16,108 @@ submitCommentVanilla = () => {
   document.getElementById("commentError").innerHTML = errorMsg;
 };
 
-function isInt(str) {
-  var val = parseInt(str);
-  return val > 0;
-}
+previewGooVanilla = () => {
+  var displayName = document.getElementById("displayName").value;
+  var category = document.getElementById("category").value;
+  var description = document.getElementById("description").value;
+  var youtube = document.getElementById("youtube").value;
+  var otherLink = document.getElementById("otherLink").value;
+  var isValid = checkAddInput(
+    displayName,
+    category,
+    description,
+    youtube,
+    otherLink
+  );
+  if (isValid) {
+    var htmlLeft =
+      '<div class="card-wrap goo-loop"><div class="row goo-preview-card align-self-center"><div class="col-3 no-gutters preview-card-left"><div class="preview-card-left-content"><img src="../assets/pics/default-hand.png" alt="new guru" class="preview-card-avatar"/><div class="preview-card-score">&#8595;000&#8593;</div></div></div>';
+    var htmlRight =
+      '<div class="col-9 no-gutters preview-card-right"><div><div class="preview-card-right-header"><span class="preview-card-name name-select">' +
+      displayName +
+      '</span><span class="preview-card-category">' +
+      category +
+      '</span></div><p class="preview-card-description">' +
+      description +
+      '</p></div><div class="icon-row"><div class="icons"><span class="icon-span"><a href="' +
+      youtube +
+      '" target="_blank"><img src="../assets/icons/youtube1.png"alt="YouTube"class="icon"/></a></span><span class="icon-span"><a href="' +
+      otherLink +
+      '" target="_blank"><img src="../assets/icons/link.png"alt="website"class="icon-link"/></a></span></div></div></div></div></div>';
+    document.getElementById("preview").innerHTML = htmlLeft + htmlRight;
+    window.scrollTo(0, 10000);
+  } else {
+    document.getElementById("preview").innerHTML = "";
+  }
+};
 
-function checkPattern(str) {
-  // test if str matches the pattern
-  var pattern = new RegExp("[a-z]{2,3}[1-9][a-z]{1,3}");
-  var match_test = pattern.test(str);
-  return match_test;
-}
+submitGooVanilla = () => {
+  var displayName = document.getElementById("displayName").value;
+  var category = document.getElementById("category").value;
+  var description = document.getElementById("description").value;
+  var youtube = document.getElementById("youtube").value;
+  var otherLink = document.getElementById("otherLink").value;
+  var isValid = checkAddInput(
+    displayName,
+    category,
+    description,
+    youtube,
+    otherLink
+  );
+  if (isValid) {
+    document.getElementById("displayName").value = "";
+    document.getElementById("category").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("youtube").value = "";
+    document.getElementById("otherLink").value = "";
+    document.getElementById("preview").innerHTML =
+      "Thank you for your submission. We look forward to reviewing it.";
+  }
+};
+
+checkAddInput = (displayName, category, description, youtube, otherLink) => {
+  isValid = true;
+  if (displayName.length < 1) {
+    isValid = false;
+    document.getElementById("name_error").innerHTML = "Add a name.";
+  } else {
+    document.getElementById("name_error").innerHTML = "";
+  }
+  if (category === "") {
+    isValid = false;
+    document.getElementById("category_error").innerHTML = "Select a category.";
+  } else {
+    document.getElementById("category_error").innerHTML = "";
+  }
+  if (description.length < 30) {
+    isValid = false;
+    document.getElementById("description_error").innerHTML =
+      "Description must be 30 characters.";
+  } else {
+    document.getElementById("description_error").innerHTML = "";
+  }
+  var checkYoutube = function(youtube) {
+    var pattern = new RegExp("(?:https?://)?(?:www.)?youtube.com/.*");
+    var checkPattern = pattern.test(youtube);
+    return checkPattern;
+  };
+  if (!checkYoutube(youtube)) {
+    isValid = false;
+    document.getElementById("youtube_error").innerHTML =
+      "Paste full youtube link, e.g., www.youtube.com/dropshipking";
+    console.log(checkYoutube(youtube));
+  } else {
+    document.getElementById("youtube_error").innerHTML = "";
+  }
+  if (otherLink.length < 1) {
+    isValid = false;
+    document.getElementById("otherLink_error").innerHTML =
+      "Other link required.";
+  } else {
+    document.getElementById("otherLink_error").innerHTML = "";
+  }
+  return isValid;
+};
 
 // Four tests:
 // 1) Age is an integer
